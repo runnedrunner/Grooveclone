67711cc0673736c8963f7d0f9c9642292fb672f9
runnedrunner <auszhou@nuevaschool.org>
2017-03-27 15:11:14 -0700

    testerinos

--- index.ios.js
+++ index.ios.js
@@ -13,7 +13,7 @@ var styles = ReactNative.StyleSheet.create({
 
 class PropertyFinderApp extends React.Component {
   render() {
-    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello Austin!! I am your Monster, GrooveClone! ");
+    return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
   }
 }
 
