74d1b987bc2297236f809414afa531e5bc189515
runnedrunner <auszhou@nuevaschool.org>
2017-03-28 09:49:30 -0700

    Searchpage sucks

--- index.ios.js
+++ index.ios.js
@@ -1,20 +1,36 @@
 'use strict';
 var React = require('react');
 var ReactNative = require('react-native');
+var SearchPage = require('./SearchPage');
+
 
 var styles = ReactNative.StyleSheet.create({
   text: {
-    color: 'red',
-    backgroundColor: 'pink',
+    color: 'black',
+    backgroundColor: 'white',
     fontSize: 30,
     margin: 80
+  },
+  container: {
+    flex: 1
   }
 });
 
-class PropertyFinderApp extends React.Component {
+class HelloWorld extends React.Component {
   render() {
     return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
   }
 }
-
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage
+        }}/>
+    );
+  }
+}
 ReactNative.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
